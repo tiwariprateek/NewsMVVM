12b45574d3a01724ff2a0ec957254df50b95c01c
tiwariprateek <prateektiwari5399@gmail.com>
2020-06-04 20:16:59 +0530

    SearchFragment completed implementation

--- app/src/main/java/com/example/newsmvvm/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/newsmvvm/ui/NewsViewModel.kt
@@ -14,6 +14,11 @@ class NewsViewModel(
 ):ViewModel(){
     val breakingNews : MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
+
+    val searchNews : MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var searchNewsPage=1
+
+
     init {
         getBreakingNews("us")
     }
@@ -23,8 +28,22 @@ class NewsViewModel(
         val response=newsRepository.getBreakingNews(countryCode,breakingNewsPage)
         breakingNews.postValue(handleBreakingNewsResponse(response))
     }
+    fun searchNews(searchQuery:String)=viewModelScope.launch {
+        searchNews.postValue(Resource.Loading())
+        val response = newsRepository.searchNews(searchQuery,searchNewsPage)
+        searchNews.postValue(handleBreakingNewsResponse(response))
+    }
 
     private fun handleBreakingNewsResponse(response:Response<NewsResponse>) : Resource<NewsResponse>{
+        if (response.isSuccessful){
+            response.body()?.let {newsResponse ->
+                return Resource.Success(newsResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    private fun handleSearchNewsResponse(response:Response<NewsResponse>) : Resource<NewsResponse>{
         if (response.isSuccessful){
             response.body()?.let {newsResponse ->
                 return Resource.Success(newsResponse)
--- app/src/main/java/com/example/newsmvvm/util/Constants.kt
+++ app/src/main/java/com/example/newsmvvm/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY="f478e52c793f4e5dbd2025c61ff1ae22"
         const val BASE_URL="https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
